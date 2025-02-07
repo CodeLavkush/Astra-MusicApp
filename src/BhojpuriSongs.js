b1d08f6b50540e678857685d05a229b24b7a41c0
CodeLavkush <mauryalavkush19@gmail.com>
2025-02-07 17:11:42 +0530

    changes in url of picsum

--- src/BhojpuriSongs.js
+++ src/BhojpuriSongs.js
@@ -3,77 +3,77 @@ const BhojpuriSongsDB = [
         songName: "Palang Sagwan Ke",
         songArtist: "Khesari Lal",
         songSrc: "./assets/Track/Bhojpuri/Palang Sagwan Ke.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/igQCv_Y33NI?si=Bp28FEyV05GRM9VL"
     },
     {
         songName: "Raate Diya Buta",
         songArtist: "Pawn Singh",
         songSrc: "./assets/Track/Bhojpuri/Raate Diya Buta.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/01vqerVBt2w?si=FIr7Q7eq6QP0gXHZ"
     },
     {
         songName: "Chhalakata Hamro",
         songArtist: "Pawn Singh",
         songSrc: "./assets/Track/Bhojpuri/Chhalakata Hamro Jawaniya.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/c4JD7rEtIj8?si=7f-u5nO-CWZS_K7j"
     },
     {
         songName: "hook raja ji",
         songArtist: "Arvind akela",
         songSrc: "./assets/Track/Bhojpuri/hook raja ji.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/gmyRw66w4uA?si=O9Q6SN8TULuzgzdL"
     },
     {
         songName: "Lens Neela Neela",
         songArtist: "Ravi Kishan",
         songSrc: "./assets/Track/Bhojpuri/Lens Neela Neela.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/ria5RNrIa1M?si=RwCRFpV2vhA-tFMH"
     },
     {
         songName: "Lolipop",
         songArtist: "Pawn Singh",
         songSrc: "./assets/Track/Bhojpuri/Lolipop.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/Gr8G_ldltDE?si=HKY4benXsvD-FLhz"
     },
     {
         songName: "Pagal Banaibe",
         songArtist: "Khesari Lal",
         songSrc: "./assets/Track/Bhojpuri/Pagal Banaibe.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/T8AvyTTcTAk?si=8nRkxkSP3i4ikd4p"
     },
     {
         songName: "Pahin ke Chali Bikini",
         songArtist: "Purav Jha",
         songSrc: "./assets/Track/Bhojpuri/Pahin ke Chali Bikini.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/2CnatpIv8qQ?si=A0NGXkUwuTDyziDi"
     },
     {
         songName: "Saaj k swar k",
         songArtist: "Khesari Lal",
         songSrc: "./assets/Track/Bhojpuri/Saaj k swar k.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/y3Jc2kxaqdw?si=Q2fYlPpreBufNPSz"
     },
     {
         songName: "Maroon color saree",
         songArtist: "Kalpana & Neelkamal Singh",
         songSrc: "./assets/Track/Bhojpuri/Maroon color saree.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/zmwfd8x0DrM?si=JCxNfeIQT9a8OIAY"
     },
     {
         songName: "Othlali se roti bor ke",
         songArtist: "Khesari Lal",
         songSrc: "./assets/Track/Bhojpuri/Othlali se roti bor ke.mp3",
-        songPoster: "https://www.picsum.photos/200x200",
+        songPoster: "https://picsum.photos/200x200",
         ytLink: "https://youtu.be/1VlhuXwhcZk?si=hjb03yxGOI66q92e"
     }
 ];
